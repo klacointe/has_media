dc91bc22f37c1133312c0b6edacef458cf6bdb99
klacointe <kevinlacointe@gmail.com>
2010-08-27 12:22:47 +0200

    deprecated named_scope

--- lib/has_media/models/medium.rb
+++ lib/has_media/models/medium.rb
@@ -31,7 +31,7 @@ class Medium < ActiveRecord::Base
   after_initialize  :set_default_encoding_status
 
 
-  named_scope :with_context, lambda {|context|
+  scope :with_context, lambda {|context|
     { :conditions => { :context => context.to_s} }
   }
 
