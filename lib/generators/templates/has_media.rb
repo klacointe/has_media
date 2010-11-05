59c6b31c0d879fc0642ee6dc070df3237ad1bbc0
klacointe <kevinlacointe@gmail.com>
2010-11-05 17:30:38 +0100

    update documentation and initializer generator

--- lib/generators/templates/has_media.rb
+++ lib/generators/templates/has_media.rb
@@ -4,10 +4,16 @@ HasMedia.directory_path = "media"
 # Set the base uri to access media
 HasMedia.directory_uri = "/media"
 
-# Set the allowed medium types for your application (used if no :only option given)
-#HasMedia.medium_types = [Image, Video, Audio]
+# Set the allowed medium types for you application
+# Set the allowed mime types for each medium type
+#HasMedia.medium_types = {
+#  "Image" => ["image/jpeg", "image/png"],
+#  "Video" => ["video/mp4"],
+#  "Audio" => ["audio/mp3"]
+#}
 
-# Set the extension of encoded files to use for each medium types (used in file_uri and file_path)
+# Set the extension of encoded files to use for each medium types 
+# This is used in file_uri and file_path
 #HasMedia.encoded_extensions = {
 #  :image => 'png',
 #  :audio => 'ogg',
