f45f5e6d13831c3615fff0d4961f45d8b81e633f
klacointe <kevinlacointe@gmail.com>
2010-12-15 19:17:10 +0100

    really fix spec ;)

--- spec/has_media_spec.rb
+++ spec/has_media_spec.rb
@@ -50,19 +50,19 @@ describe "HasMedia" do
     end
 
     it "should define accessors" do
-      @medium.methods.should include(:image)
-      @medium.methods.should include(:images)
-      @medium.methods.should include(:audio)
-      @medium.methods.should include(:audios)
-      @medium.methods.should include(:pdf)
+      @medium.methods.map{|m| m.to_sym}.should include(:image)
+      @medium.methods.map{|m| m.to_sym}.should include(:images)
+      @medium.methods.map{|m| m.to_sym}.should include(:audio)
+      @medium.methods.map{|m| m.to_sym}.should include(:audios)
+      @medium.methods.map{|m| m.to_sym}.should include(:pdf)
     end
 
     it "should define setters" do
-      @medium.methods.should include(:image=)
-      @medium.methods.should include(:images=)
-      @medium.methods.should include(:audio=)
-      @medium.methods.should include(:audios=)
-      @medium.methods.should include(:pdf=)
+      @medium.methods.map{|m| m.to_sym}.should include(:image=)
+      @medium.methods.map{|m| m.to_sym}.should include(:images=)
+      @medium.methods.map{|m| m.to_sym}.should include(:audio=)
+      @medium.methods.map{|m| m.to_sym}.should include(:audios=)
+      @medium.methods.map{|m| m.to_sym}.should include(:pdf=)
     end
 
     it "should associate image to mediated object" do
