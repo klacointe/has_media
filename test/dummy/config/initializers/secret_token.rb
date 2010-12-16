b2aeffabdf6c59f1ec5b8b268e5b809d676160c2
klacointe <kevinlacointe@gmail.com>
2010-12-16 18:56:50 +0100

    remove test unit directory

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'dcdae85e1d37ea1f2e3da8820c8be55db3d170ee6bcd470f2d4dafe3d2c3c69ff685e87d5b6d22e55fa5e089f8dfa4dda5e484f9f6105b50fdf53a010a1e7834'
--- test/has_media_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HasMediaTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, HasMedia
-  end
-end
